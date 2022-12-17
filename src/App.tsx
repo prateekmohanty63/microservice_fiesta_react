d2ddc5a9df14585f9687478ce075e7f0ef0ab9f4
Prateek <prateekmohanty63@gmail.com>
2022-12-17 19:08:26 +0530

    Added admin || main

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,34 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import Menu from './components/Menu';
+import Product from './admin/Products';
 
 function App() {
   return (
     <div className="App">
       <body cz-shortcut-listen="true">
-    
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
-      <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <input className="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search" />
-      <div className="navbar-nav">
-        <div className="nav-item text-nowrap">
-          <a className="nav-link px-3" href="#">Sign out</a>
-        </div>
-      </div>
-    </header>
-    
-    <div className="container-fluid">
-      <div className="row">
-        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-          <div className="position-sticky pt-3 sidebar-sticky">
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home align-text-bottom" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                  Dashboard
-                </a>
-              </li>
-             
-           
-            </ul>
-          </div>
-        </nav>
-    
-        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
-     
-    
-          <h2>Section title</h2>
-          <div className="table-responsive">
-            <table className="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-              
-              </tbody>
-            </table>
-          </div>
-        </main>
-      </div>
-    </div>
-    
-    
 
-      
-    
-    </body>
+      <Nav />
+
+        <div className="container-fluid">
+          <div className="row">
+           
+           <Menu />
+
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
+
+
+             <Product />
+            </main>
+          </div>
+        </div>
+
+
+
+
+
+      </body>
     </div>
   );
 }
